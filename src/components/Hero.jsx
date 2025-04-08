3c9ac8d4202cd2bfc17d1e8d4f445f9d4f10ca5d
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-04-08 11:38:33 +0300

    Hero componentine navbar ve slider kismi eklendi

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TopNavbar from "../layout/TopNavbar";
+import NavBar from "../layout/NavBar";
+import TopSlider from "./TopSlider";
+
+function Hero() {
+  return (
+    <div>
+      <TopNavbar />
+      <NavBar />
+      <TopSlider />
+    </div>
+  );
+}
+
+export default Hero;
