17b3d6ab67a46efa6ebd74b39e811f943fe4116c
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-04-08 11:19:43 +0300

    Tum resimler dosyada toplandi, katerorilere ayrildi

---
+++ src/assets/Images.jsx
@@ -0,0 +1,115 @@
+import slider1 from "./images/slider1.jpg";
+import slider2 from "./images/slider2.png";
+import blueEllipse from "../assets/shapes/Ellipse 14.svg";
+import greenEllipse from "../assets/shapes/Ellipse 15.svg";
+import orangeEllipse from "../assets/shapes/Ellipse 16.svg";
+import blackEllipse from "../assets/shapes/Ellipse 17.svg";
+import productCard1 from "../assets/images/productImages/product-cover-5-1.png";
+import productCard2 from "../assets/images/productImages/product-cover-5-2.png";
+import productCard3 from "../assets/images/productImages/product-cover-5-3.png";
+import productCard4 from "../assets/images/productImages/product-cover-5-4.png";
+import productCard5 from "../assets/images/productImages/product-cover-5-5.png";
+import productCard6 from "../assets/images/productImages/product-cover-5-6.png";
+import productCard7 from "../assets/images/productImages/product-cover-5-7.png";
+import productCard8 from "../assets/images/productImages/product-cover-5.png";
+
+import featured1 from "../assets/images/featuredImages/car.png";
+import featured2 from "../assets/images/featuredImages/home.png";
+import featured3 from "../assets/images/featuredImages/umbrella.png";
+import vectorArrow from "../assets/shapes/icon arrow-next.svg";
+import nextVector from "../assets/shapes/icn arrow-right icn-xs.svg";
+
+import hooli from "../assets/icons/Vector.png";
+import lyft from "../assets/icons/Vector-1.png";
+import spring from "../assets/icons/col-md-2.png";
+import stripe from "../assets/icons/Vector-2.png";
+import amazon from "../assets/icons/Vector-3.png";
+import reddit from "../assets/icons/Vector-4.png";
+
+import productList1 from "../assets/images/productList/fixed-height-1.png";
+import productList2 from "../assets/images/productList/fixed-height-2.png";
+import productList3 from "../assets/images/productList/fixed-height-3.png";
+import productList4 from "../assets/images/productList/fixed-height-4.png";
+import productList5 from "../assets/images/productList/fixed-height-5.png";
+import productList6 from "../assets/images/productList/fixed-height-6.png";
+import productList7 from "../assets/images/productList/fixed-height-7.png";
+import productList8 from "../assets/images/productList/fixed-height-8.png";
+import productList9 from "../assets/images/productList/fixed-height-9.png";
+import productList10 from "../assets/images/productList/fixed-height-10.png";
+import productList11 from "../assets/images/productList/fixed-height.png";
+import productList12 from "../assets/images/productList/product-cover-5.png";
+
+import category1 from "../assets/images/categories/card-cover-5.png";
+import category2 from "../assets/images/categories/media bg-cover.png";
+import category3 from "../assets/images/categories/media bg-cover2.png";
+import category4 from "../assets/images/categories/media bg-cover3.png";
+import category5 from "../assets/images/categories/media bg-cover4.png";
+
+export const Images = {
+  sliders: {
+    slider1,
+    slider2,
+  },
+  
+  icons: {
+    hooli,
+    lyft,
+    spring,
+    stripe,
+    amazon,
+    reddit,
+  },
+
+  shapes: {
+    ellipses: {
+      blueEllipse,
+      greenEllipse,
+      orangeEllipse,
+      blackEllipse,
+    },
+    vectors: {
+      vectorArrow,
+      nextVector,
+    },
+  },
+
+  categories: {
+    category1,
+    category2,
+    category3,
+    category4,
+    category5,
+  },
+
+  homeProductCard: {
+    productCard1,
+    productCard2,
+    productCard3,
+    productCard4,
+    productCard5,
+    productCard6,
+    productCard7,
+    productCard8,
+  },
+
+  featuredPosts: {
+    featured1,
+    featured2,
+    featured3,
+  },
+
+  productList: {
+    productList1,
+    productList2,
+    productList3,
+    productList4,
+    productList5,
+    productList6,
+    productList7,
+    productList8,
+    productList9,
+    productList10,
+    productList11,
+    productList12,
+  },
+};
