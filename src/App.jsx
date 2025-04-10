ff0aebfcc94676943087765a1a1fe8634a0e57c5
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-04-10 17:32:16 +0300

    Appe about page eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import HomePage from "./pages/HomePage";
+import About from "./pages/About";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -8,6 +9,9 @@ function App() {
       <Switch>
         <Route exact path="/">
           <HomePage />
+        </Route>
+        <Route exact path="/about">
+          <About />
         </Route>
       </Switch>
     </Router>
