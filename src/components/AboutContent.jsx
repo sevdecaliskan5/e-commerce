8da57bfee6c61fa430919545b6f2fee01f908c72
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-04-10 17:52:42 +0300

    Update AboutContent

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -2,18 +2,20 @@ import React from "react";
 
 const AboutContent = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between gap-8 md:gap-0 py-6 px-4 md:px-0">
-       <div className="w-full md:w-[390px] flex flex-col items-center md:items-start gap-4 md:gap-6">
-        <p className="text-[#E74040] text-sm font-normal">Problems trying</p>
-       <p className="text-[#252B42] text-2xl md:text-3xl font-bold text-center md:text-start max-w-[300px] md:max-w-none">
-          Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.
-        </p>
-      </div>
-      <div className="w-full md:w-[520px]">
-      <p className="text-[#737373] font-normal text-sm text-center md:text-start max-w-[300px] md:max-w-none mx-auto md:mx-0">
-          Problems trying to resolve the conflict between the two major realms
-          of Classical physics: Newtonian mechanics{" "}
-        </p>
+    <div className="w-full flex justify-center">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 md:gap-0 py-6 px-4 md:px-0 w-full md:w-[1018px]">
+        <div className="w-full md:w-[390px] flex flex-col items-center md:items-start gap-4 md:gap-6">
+          <p className="text-[#E74040] text-sm font-normal">Problems trying</p>
+          <p className="text-[#252B42] text-2xl md:text-3xl font-bold text-center md:text-start max-w-[300px] md:max-w-none">
+            Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.
+          </p>
+        </div>
+        <div className="w-full md:w-[520px]">
+          <p className="text-[#737373] font-normal text-sm text-center md:text-start max-w-[300px] md:max-w-none mx-auto md:mx-0">
+            Problems trying to resolve the conflict between the two major realms
+            of Classical physics: Newtonian mechanics{" "}
+          </p>
+        </div>
       </div>
     </div>
   );
