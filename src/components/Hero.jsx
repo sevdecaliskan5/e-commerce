e251cceb0f6c3255dd3764d3c2c64ff23d0ca1b0
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-04-10 17:14:25 +0300

    Slider heroya eklendi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,49 @@
 import React from "react";
 import TopNavbar from "../layout/TopNavbar";
 import NavBar from "../layout/NavBar";
-import TopSlider from "./TopSlider";
+import Images from "../assets/Images";
+import Slider from './Slider';
 
-function Hero() {
+const heroSlides = [
+   {
+     id: 1,
+     image: Images.sliders.sliderTop1,
+     title: "NEW COLLECTION",
+     subtitle: "SUMMER 2025",
+     description: "We know how large objects will act, but things on a small scale.",
+     buttonText: "SHOP NOW",
+     buttonLink: "/productlist",
+     altText: "Summer Collection 2025"
+   },
+   {
+     id: 2,
+     image: Images.sliders.sliderTop2,
+     title: "NEW COLLECTION",
+     subtitle: "SUMMER 2025",
+     description: "We know how large objects will act, but things on a small scale.",
+     buttonText: "SHOP NOW",
+     buttonLink: "/productlist",
+     altText: "Summer Collection 2025"
+   }
+ ];
+
+ function Hero() {
   return (
     <div>
       <TopNavbar />
       <NavBar />
-      <TopSlider />
-    </div>
-  );
-}
-
-export default Hero;
+      <Slider 
+         slides={heroSlides}
+         containerClassName="relative w-full h-screen overflow-hidden"
+         slideClassName="w-full h-screen flex-shrink-0 relative"
+         imageClassName="w-full h-full object-cover"
+         contentClassName="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white p-4"
+         buttonClassName="inline-block bg-[#2DC071] px-8 py-3 rounded text-white font-bold hover:bg-[#23856D] transition-colors duration-300"
+         showIndicators={true}
+         showNavigation={true}
+         autoPlay={true}
+         autoPlayInterval={5000}
+       />
+     </div>
+   );
+ }
