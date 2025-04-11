cef7b6bd0f0ba9d7cb93c5f474dc6efa53ea2054
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-04-11 13:58:15 +0300

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -3,50 +3,51 @@ import { AllData } from "../assets/AllData";
 import { Facebook, Twitter, Instagram } from "lucide-react";
 
 const Team = () => {
-const items = AllData.team;
+  const items = AllData.team;
   return (
     <div>
       <div className="flex flex-col gap-28 text-[#252B42] text-center items-center py-28 ">
         <div className="flex flex-col gap-2">
           <h1 className="px-20 sm:px-0 font-bold text-[40px]">Meet Our Team</h1>
         </div>
-        <div className="flex flex-wrap justify-between gap-6 w-full">
-
-            {items.map((item) => (
-            <div key={item.id} className="w-full sm:w-[30%] lg:w-[30%] font-bold flex flex-col items-center">
-
-                <img
-                  className="w-full h-[231px] object-cover"
-                  src={item.img}
-                  alt={item.header}
-                />
-                <div className="flex flex-col gap-[10px] p-[30px]">
-                  <h5 className="text-base">{item.header}</h5>
-                  <h6 className="text-sm">{item.role}</h6>
-                  <div className="flex gap-5 justify-center">
-                    <a
-                      href={item.facebook}
-                      className="text-[#335BF5] hover:text-[#252B42] transition-colors"
-                    >
-                      <Facebook size={24} />
-                    </a>
-                    <a
-                      href={item.instagram}
-                      className="text-[#E51F5A] hover:text-[#252B42] transition-colors"
-                    >
-                      <Instagram size={24} />
-                    </a>
-                    <a
-                      href={item.twitter}
-                      className="text-primary-color hover:text-[#252B42] transition-colors"
-                    >
-                      <Twitter size={24} />
-                    </a>
-                  </div>
+        <div className="w-[1034px] mx-auto flex flex-wrap justify-between gap-[30px]">
+          {items.map((item) => (
+            <div
+              key={item.id}
+              className="w-full sm:w-[30%] lg:w-[30%] font-bold flex flex-col items-center"
+            >
+              <img
+                className="w-[316px] h-[231px] object-cover"
+                src={item.img}
+                alt={item.header}
+              />
+              <div className="flex flex-col gap-[10px] p-[30px]">
+                <h5 className="text-base">{item.header}</h5>
+                <h6 className="text-sm">{item.role}</h6>
+                <div className="flex gap-5 justify-center">
+                  <a
+                    href={item.facebook}
+                    className="text-[#335BF5] hover:text-[#252B42] transition-colors"
+                  >
+                    <Facebook size={24} />
+                  </a>
+                  <a
+                    href={item.instagram}
+                    className="text-[#E51F5A] hover:text-[#252B42] transition-colors"
+                  >
+                    <Instagram size={24} />
+                  </a>
+                  <a
+                    href={item.twitter}
+                    className="text-primary-color hover:text-[#252B42] transition-colors"
+                  >
+                    <Twitter size={24} />
+                  </a>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Team from "../components/Team";
+import InnerHeader from "../layout/InnerHeader";
 import TeamHeader from "../components/TeamHeader";
 import TeamHeroPictures from "../components/TeamHeroPictures";
 import TeamCTA from "../components/TeamCTA";
+import Footer from "../layout/Footer"
 
 export default function TeamPage() {
   return (
     <>
+      <InnerHeader />
       <TeamHeader />
       <TeamHeroPictures />
       <Team />
       <TeamCTA />
+      <Footer/>
     </>
   );
 }
