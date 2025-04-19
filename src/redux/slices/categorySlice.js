430f7d77b324bb3296abddc336c334b05423ceed
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-04-19 13:44:19 +0300

    Add async Redux slice for category fetching

---
+++ src/redux/slices/categorySlice.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchCategories = createAsyncThunk(
+  "categories/fetchCategories",
+  async () => {
+    const response = await axios.get("https://workintech-fe-ecommerce.onrender.com/categories");
+    return response.data;
+  }
+);
+
+const categorySlice = createSlice({
+  name: "categories",
+  initialState: {
+    items: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = action.payload;
+      })
+      .addCase(fetchCategories.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default categorySlice.reducer;
+
